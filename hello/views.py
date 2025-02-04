9c30870c5a8d03ca2d44a02e5eb2f46bc741d4c2
mocha8686 <june.mocha8686@gmail.com>
2025-02-04 11:24:54 -0800

    feat: hello app

---
+++ hello/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(req: HttpRequest) -> HttpResponse:
+    return HttpResponse(b"Hello, world!")
