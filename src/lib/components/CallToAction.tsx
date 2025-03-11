829fdeb693773a0add1a6e5b9e2e4edd6bea7b5c
mocha8686 <june.mocha8686@gmail.com>
2025-03-11 10:40:54 -0700

    refactor: clsx

--- src/lib/components/CallToAction.tsx
+++ src/lib/components/CallToAction.tsx
@@ -2,6 +2,7 @@ import { Icon, type IconifyIconProps } from '@iconify/react';
 import type React from 'react';
 import './CallToAction.css';
 import { Link } from 'react-router';
+import clsx from 'clsx';
 
 export interface CallToActionProps
 	extends React.ComponentPropsWithoutRef<typeof Link> {
@@ -17,9 +18,9 @@ export default function CallToAction({
 	const iconProps = typeof icon === 'string' ? { icon } : icon;
 
 	return (
-		<Link className={`${className} CallToAction`} {...props}>
+		<Link className={clsx(className, 'CallToAction')} {...props}>
 			{/* @ts-expect-error Icon uses an internal interface for its props which is compatible with this one, but we can't access it */}
-			<Icon className="CallToAction-icon" {...iconProps} />
+			<Icon className='CallToAction-icon' {...iconProps} />
 			{children}
 		</Link>
 	);
