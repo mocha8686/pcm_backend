9b4e2967123e50c5e95d7d012c885e6ca7f1d0bc
mocha8686 <june.mocha8686@gmail.com>
2025-03-11 14:06:30 -0700

    refactor: extract contact form

--- src/lib/components/Footer.tsx
+++ src/lib/components/Footer.tsx
@@ -1,7 +1,7 @@
 import './Footer.css';
 import logo from '$lib/assets/pcm_white_accent.jpg';
 import { Link } from 'react-router';
-import CallToAction from './CallToAction';
+import ContactForm from './ContactForm';
 
 export default function Footer() {
 	return (
@@ -20,51 +20,5 @@ export default function Footer() {
 				</section>
 			</footer>
 		</>
-	);
-}
-
-function ContactForm() {
-	return (
-		<form className='ContactForm' action='/'>
-			<h1 className='ContactForm-header'>Send Us a Message</h1>
-
-			<label className='ContactForm-label' htmlFor='footer-name'>
-				Name
-			</label>
-			<input
-				className='ContactForm-input'
-				type='text'
-				name='name'
-				id='contactForm-name'
-				placeholder='John Doe'
-			/>
-
-			<label className='ContactForm-label' htmlFor='contactForm-email'>
-				Email
-			</label>
-			<input
-				className='ContactForm-input'
-				type='email'
-				name='email'
-				id='contactForm-email'
-				placeholder='person@example.com'
-			/>
-
-			<label className='ContactForm-label' htmlFor='contactForm-message'>
-				Message
-			</label>
-			<textarea
-				className='ContactForm-input ContactForm-input--textarea'
-				name='message'
-				id='contactForm-message'
-				placeholder='Hello!'
-				rows={6}
-				cols={28}
-			/>
-
-			<CallToAction className='ContactForm-submit' ctaType='button' type='submit' icon='tabler:mail-fast'>
-				Send Your Message
-			</CallToAction>
-		</form>
 	);
 }
